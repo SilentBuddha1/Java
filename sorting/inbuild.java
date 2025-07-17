53ea57b4ac4fe976c6745b1adad1d887a1f9c446
silentbuddha1 <iamramchandrakarki@gmail.com>
2025-07-17 23:55:58 +0545

    inbuild Sorting

---
+++ sorting/inbuild.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class inbuild{
+	public static void main(String args[]){
+		int num[] = {5,4,1,3,2};
+		Arrays.sort(num);
+
+		for(int i = 0; i < num.length; i++){
+			System.out.print(num[i] + " " );
+		}
+	}
+}
+
